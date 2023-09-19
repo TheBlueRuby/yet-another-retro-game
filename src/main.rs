a9870d2db1b93f009384378203eba78f1f6cf184
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2023-09-20 01:18:45 +1000

    pixel-perfect scaling and textures

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,103 @@ mod types;
 use player::*;
 
 fn main() {
-    /* ---------------------------------- init ---------------------------------- */
+    /* -------------------------------------------------------------------------- */
+    /*                                    init                                    */
+    /* -------------------------------------------------------------------------- */
+
+    /* ------------------------------- raylib init ------------------------------ */
+    let screen_width = 1920;
+    let screen_height = 1080;
+
+    let virt_screen_width = 320;
+    let virt_screen_height = 180;
+
+    let virt_ratio: f32 = (screen_width as f32) / (virt_screen_width as f32);
+
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
+        .size(screen_width, screen_height)
         .title("Yet Another Retro Game")
         .build();
 
     rl.set_target_fps(60);
 
-    let mut player: Player = Player::new(0, 0, 8, 8, 4, 4);
+    let mut screen_camera = Camera2D {
+        target: Vector2::new(screen_width as f32 / 2.0, screen_height as f32 / 2.0),
+        offset: Vector2::new(0.0, 0.0),
+        rotation: 0.0,
+        zoom: 1.0,
+    };
 
-    /* -------------------------------- main loop ------------------------------- */
+    let mut world_camera = Camera2D {
+        target: Vector2::new(screen_width as f32 / 2.0, screen_height as f32 / 2.0),
+        offset: Vector2::new(0.0, 0.0),
+        rotation: 0.0,
+        zoom: 1.0,
+    };
+
+    let mut virt_screen: RenderTexture2D = rl
+        .load_render_texture(&thread, virt_screen_width, virt_screen_height)
+        .unwrap();
+    let source_rec = Rectangle::new(
+        0.0,
+        0.0,
+        virt_screen.texture.width as f32,
+        -virt_screen.texture.height as f32,
+    );
+    let dest_rec = Rectangle::new(
+        -virt_ratio,
+        -virt_ratio,
+        screen_width as f32 + (virt_ratio * 2.0),
+        screen_height as f32 + (virt_ratio * 2.0),
+    );
+
+    /* -------------------------------- game init ------------------------------- */
+    let mut player: Player = Player::new(0, 0, 8, 8, 4, 4, Image::load_image("resources/player.png").unwrap());
+    let player_tex = rl.load_texture_from_image(&thread, &player.texture).unwrap();
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  main loop                                 */
+    /* -------------------------------------------------------------------------- */
     while !rl.window_should_close() {
-        player.handle_input(&mut rl);
+        /* --------------------------------- update --------------------------------- */
+        player.handle_input(&rl);
+
+        /* ------------------------------ camera stuff ------------------------------ */
+        world_camera.target.x = screen_camera.target.x;
+        screen_camera.target.x -= world_camera.target.x;
+        screen_camera.target.x *= virt_ratio;
+
+        world_camera.target.y = screen_camera.target.y;
+        screen_camera.target.y -= world_camera.target.y;
+        screen_camera.target.y *= virt_ratio;
 
         /* --------------------------------- drawing -------------------------------- */
-        let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::WHITE);
-        draw(d, player);
-    }
-}
+        let mut draw_handle = rl.begin_drawing(&thread);
 
-fn draw(mut d: RaylibDrawHandle<'_>, player: Player) {
-    d.draw_rectangle(player.transform.x, player.transform.y, 25, 25, Color::BLUE);
+        let mut texture_mode = draw_handle.begin_texture_mode(&thread, &mut virt_screen);
+        let mut w_mode_2d = texture_mode.begin_mode2D(world_camera);
+        w_mode_2d.clear_background(Color::WHITE);
+        drop(w_mode_2d);
+        player.draw(&mut texture_mode, &player_tex);
+
+        drop(texture_mode);
+
+        draw_handle.clear_background(Color::MAGENTA);
+        let mut s_mode_2d = draw_handle.begin_mode2D(screen_camera);
+        s_mode_2d.draw_texture_pro(
+            &virt_screen,
+            source_rec,
+            dest_rec,
+            Vector2 { x: 0.0, y: 0.0 },
+            0.0,
+            Color::WHITE,
+        );
+        drop(s_mode_2d);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   cleanup                                  */
+    /* -------------------------------------------------------------------------- */
+    drop(virt_screen);
+    drop(player_tex);
 }
--- src/player.rs
+++ src/player.rs
@@ -1,20 +1,30 @@
-use raylib::prelude::KeyboardKey::*;
+use raylib::prelude::{KeyboardKey::*, RaylibDraw};
 
 use crate::types::Entity;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub struct Player {
     pub(crate) transform: Entity,
+    pub(crate) texture: raylib::texture::Image,
 }
 
 impl Player {
-    pub fn new(x: i32, y: i32, mv_x: i32, mv_y: i32, sp_x: i32, sp_y: i32) -> Self {
+    pub fn new(
+        x: i32,
+        y: i32,
+        mv_x: i32,
+        mv_y: i32,
+        sp_x: i32,
+        sp_y: i32,
+        texture: raylib::texture::Image,
+    ) -> Self {
         Self {
             transform: Entity::new(x, y, 0, 0, mv_x, mv_y, sp_x, sp_y),
+            texture,
         }
     }
 
-    pub fn handle_input(&mut self, rl: &mut raylib::RaylibHandle) {
+    pub fn handle_input(&mut self, rl: &raylib::RaylibHandle) {
         if rl.is_key_down(KEY_W) && self.transform.vel_y >= -self.transform.mv_y {
             self.transform.vel_y -= self.transform.sp_y;
         }
@@ -47,5 +57,19 @@ impl Player {
 
         self.transform.x += self.transform.vel_x;
         self.transform.y += self.transform.vel_y;
+    }
+
+    pub fn draw(
+        &self,
+        t: &mut raylib::prelude::RaylibTextureMode<'_, raylib::prelude::RaylibDrawHandle<'_>>,
+        player_tex: &raylib::texture::Texture2D,
+    ) {
+        t.draw_texture_ex(
+            player_tex,
+            raylib::prelude::Vector2::new(self.transform.x as f32, self.transform.y as f32),
+            0.0,
+            1.0,
+            raylib::prelude::Color::WHITE,
+        )
     }
 }
